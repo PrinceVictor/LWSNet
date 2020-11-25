0b8d2bc290fb8f90313c1f2ffd6e56e6fca6eed3
PrinceVictor <victorchew233@gmail.com>
2020-11-25 15:05:38 +0800

    temp pass

--- train.py
+++ train.py
@@ -1,27 +1,29 @@
 import argparse
 import paddle
+import paddle.nn.functional as F
 import paddle.fluid as fluid
+from paddle.io import DataLoader
 
 from models.models import *
 from dataloader import sceneflow as sf
 from dataloader import dataloader
 
-parser = argparse.ArgumentParser(description='finetune KITTI')
+parser = argparse.ArgumentParser(description='pretrain Sceneflow main()')
 
 parser.add_argument('--maxdisp', type=int, default=192,
                     help='maxium disparity')
-parser.add_argument('--datapath', default='/home/liupengchao/zhb/dataset/sceneflow')
+parser.add_argument('--datapath', default='/home/xjtu/NAS/zhb/dataset/sceneflow/')
 parser.add_argument('--loss_weights', type=float, nargs='+', default=[0.25, 0.5, 1., 1.])
 parser.add_argument('--max_disparity', type=int, default=192)
 parser.add_argument('--maxdisplist', type=int, nargs='+', default=[24, 5, 5])
 parser.add_argument('--channels_3d', type=int, default=8, help='number of initial channels 3d feature extractor ')
 parser.add_argument('--layers_3d', type=int, default=4, help='number of initial layers in 3d network')
 parser.add_argument('--growth_rate', type=int, nargs='+', default=[4,1,1], help='growth rate in the 3d network')
-parser.add_argument('--lr', type=float, default=1e-4, help='learning rate')
+parser.add_argument('--lr', type=float, default=5e-4, help='learning rate')
 parser.add_argument('--epoch', type=int, default=10)
 parser.add_argument('--train_batch_size', type=int, default=8)
 parser.add_argument('--test_batch_size', type=int, default=16)
-parser.add_argument('--gpu_id', type=int, default=3)
+parser.add_argument('--gpu_id', type=int, default=0)
 parser.add_argument('--model', type=str, default="test")
 parser.add_argument('--resume', action='store_true', default=False,)
 
@@ -29,7 +31,7 @@ args = parser.parse_args()
 
 def main():
 
-    print("finetune KITTI main()")
+    print("pretrain Sceneflow main()")
 
     stages = 4
     gpu_id = args.gpu_id
@@ -39,23 +41,12 @@ def main():
 
     train_left_img, train_right_img, train_left_disp, test_left_img, test_right_img, test_left_disp = sf.dataloader(args.datapath)
 
-    # train_left_img, train_right_img, train_left_disp, \
-    # test_left_img, test_right_img, test_left_disp = kitti.dataloader(args.datapath)
-
-    train_loader = dataloader.sceneflow_dataloader(train_left_img, train_right_img, train_left_disp, training=True)
-    test_loader = dataloader.sceneflow_dataloader(test_left_img, test_right_img, test_left_disp, training=False)
-
-    train_reader = train_loader.create_reader()
-    train_batch_reader = paddle.batch(reader=train_reader, batch_size=args.train_batch_size, drop_last=True)
-
-    test_reader = test_loader.create_reader()
-    test_batch_reader = paddle.batch(reader=test_reader, batch_size=args.test_batch_size, drop_last=True)
-
-    train_data_loader = fluid.io.DataLoader.from_generator(iterable=True, capacity=4)
-    train_data_loader.set_sample_list_generator(train_batch_reader, places=fluid.cuda_places(gpu_id))
-
-    test_data_loader = fluid.io.DataLoader.from_generator(iterable=True, capacity=4)
-    test_data_loader.set_sample_list_generator(test_batch_reader, places=fluid.cuda_places(gpu_id))
+    train_loader = paddle.io.DataLoader(
+        dataloader.MyDataloader(train_left_img, train_right_img, train_left_disp, training=True, kitti_set=False),
+        batch_size=args.train_batch_size, places=paddle.CUDAPlace(gpu_id), shuffle=True, drop_last=False, num_workers=2)
+    test_loader = paddle.io.DataLoader(
+        dataloader.MyDataloader(test_left_img, test_right_img, test_left_disp, training=False, kitti_set=False),
+        batch_size=args.test_batch_size, places=paddle.CUDAPlace(gpu_id), shuffle=False, drop_last=False, num_workers=2)
 
     model = Ownnet(args)
 
@@ -64,14 +55,16 @@ def main():
         model.set_dict(model_state)
 
     batch_len = 0
-    for batch_id, data in enumerate(train_data_loader()):
-        batch_len += 1
+    # for batch_id, data in enumerate(train_loader()):
+    #     batch_len += 1
 
     print("batch_len", batch_len)
 
-    boundaries = [50*batch_len, 150*batch_len, 300*batch_len]
+    boundaries = [150*batch_len, 150*batch_len, 300*batch_len]
     values = [args.lr, args.lr * 0.1, args.lr * 0.01, args.lr * 0.001]
-    adam = fluid.optimizer.Adam(learning_rate=fluid.dygraph.PiecewiseDecay(boundaries, values, 0),
+    # adam = fluid.optimizer.Adam(learning_rate=fluid.dygraph.PiecewiseDecay(boundaries, values, 0),
+    #                             parameter_list=model.parameters())
+    adam = fluid.optimizer.Adam(learning_rate=args.lr,
                                 parameter_list=model.parameters())
 
     error_3pixel_check = np.inf
@@ -84,25 +77,19 @@ def main():
 
         model.train()
 
-        for batch_id, data in enumerate(train_data_loader()):
+        for batch_id, data in enumerate(train_loader()):
             left_img, right_img, gt = data
 
-            gt_mask = np.zeros(gt.shape, np.float32)
-            gt_mask[gt.numpy() < 192] = 1.0
-            useful_pixels = len(np.flatnonzero(gt_mask))
-            gt_mask = fluid.layers.assign(gt_mask)
-            gt = fluid.layers.elementwise_mul(gt, gt_mask)
+            mask = paddle.to_tensor(gt.numpy() < args.maxdisp)
+            gt_mask = paddle.masked_select(gt, mask)
 
-            output = model(left_img, right_img)
+            outputs = model(left_img, right_img)
+            outputs = [paddle.squeeze(output) for output in outputs]
 
-            L1_loss = fluid.dygraph.L1Loss("mean")
             tem_stage_loss = []
             for index in range(stages):
-
-                mask_output = fluid.layers.elementwise_mul(output[index], gt_mask)
-                # temp_loss = fluid.layers.reduce_mean(fluid.layers.smooth_l1(mask_output, gt))
-                temp_loss = L1_loss(mask_output, gt)
-                temp_loss = temp_loss * args.loss_weights[index] * (gt.shape[-1] * gt.shape[-2]) / useful_pixels
+                temp_loss = args.loss_weights[index] * F.smooth_l1_loss(paddle.masked_select(outputs[index], mask),
+                                                                        gt_mask, reduction='mean')
                 tem_stage_loss.append(temp_loss)
                 stage_loss_list[index] += temp_loss.numpy()
 
@@ -124,32 +111,32 @@ def main():
                                                                                           np.round(sum_losses_rec / (
                                                                                                       batch_id + 1), 3),
                                                                                           info_str))
+        with fluid.dygraph.no_grad():
+            model.eval()
 
-        model.eval()
+            for batch_id, data in enumerate(test_loader()):
+                left_img, right_img, gt = data
 
-        for batch_id, data in enumerate(test_data_loader()):
-            left_img, right_img, gt = data
+                output = model(left_img, right_img)
 
-            output = model(left_img, right_img)
+                for stage in range(stages):
+                    error_3pixel_list[stage] += error_estimating(output[stage].numpy(), gt.numpy())
+                    error_3pixel = sum(error_3pixel_list)
 
-            for stage in range(stages):
-                error_3pixel_list[stage] += error_estimating(output[stage].numpy(), gt.numpy())
-                error_3pixel = sum(error_3pixel_list)
+                info_str = ['Stage {} = {:.2f}'.format(x, float(error_3pixel_list[x] / (batch_id + 1))) for x in
+                            range(stages)]
+                info_str = '\t'.join(info_str)
 
-            info_str = ['Stage {} = {:.2f}'.format(x, float(error_3pixel_list[x] / (batch_id + 1))) for x in
-                        range(stages)]
-            info_str = '\t'.join(info_str)
+                print("Test: epoch {}, batch_id {} error 3pixel {}\t  {}".format(epoch,
+                                                                                 batch_id,
+                                                                                 round(error_3pixel / (batch_id + 1), 3),
+                                                                                 info_str))
 
-            print("Test: epoch {}, batch_id {} error 3pixel {}\t  {}".format(epoch,
-                                                                             batch_id,
-                                                                             round(error_3pixel / (batch_id + 1), 3),
-                                                                             info_str))
+            if error_3pixel / (batch_id + 1) < error_3pixel_check:
+                error_3pixel_check = error_3pixel / (batch_id + 1)
 
-        if error_3pixel / (batch_id + 1) < error_3pixel_check:
-            error_3pixel_check = error_3pixel / (batch_id + 1)
-
-            fluid.save_dygraph(model.state_dict(), "results/"+args.model)
-            print("save model param success")
+                fluid.save_dygraph(model.state_dict(), "results/"+args.model)
+                print("save model param success")
 
 
 def error_estimating(disp, ground_truth, maxdisp=192):
